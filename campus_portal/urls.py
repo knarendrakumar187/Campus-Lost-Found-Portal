cc527f32d926887cfaee9c88928e0eb023d8a0d8
narendra <narendrakommineni18@gmail.com>
2026-01-31 20:09:00 +0530

    Allow serving media files in production

--- campus_portal/urls.py
+++ campus_portal/urls.py
@@ -6,9 +6,10 @@ Think of it as a "table of contents" for your website.
 """
 
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     # Admin panel URL - accessible at /admin/
@@ -17,10 +18,14 @@ urlpatterns = [
     # All URLs from lostfound app
     # When someone visits /, Django will look in lostfound/urls.py
     path('', include('lostfound.urls')),
+
+    # Serve media files in production (Hackathon specific config)
+    re_path(r'^media/(?P<path>.*)$', serve, {
+        'document_root': settings.MEDIA_ROOT,
+    }),
 ]
 
-# Serve media files during development
-# This allows uploaded images to be displayed
+# Serve static files during development if needed (WhiteNoise handles this in prod)
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
